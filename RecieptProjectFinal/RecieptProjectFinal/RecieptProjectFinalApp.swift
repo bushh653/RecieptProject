60437a2e95462fe556f85fd25920c88da2560746
Bush <bush.h@etoncollege.org.uk>
2021-02-24 14:31:06 +0000

    Creation
    Created a new XCode project preparing for the receipt app.

---
+++ RecieptProjectFinal/RecieptProjectFinal/RecieptProjectFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecieptProjectFinalApp.swift
+//  RecieptProjectFinal
+//
+//  Created by Harry Bush on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RecieptProjectFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
