cccf6454fd5336e5f70177b3c2263122b7011665
Bush <bush.h@etoncollege.org.uk>
2021-06-02 17:48:43 +0100

    Adding CoreData
    Connected Core Data to the Shopping List Items by reworking how items are added to the list and creating a Persistance file in order for it to work with @main. Then made sure that it was called in the App structure.

---
+++ RecieptProjectFinal/RecieptProjectFinal/Shopping List/SearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar.swift
+//  RecieptProjectFinal
+//
+//  Created by Harry Bush on 02/06/2021.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
+}
--- RecieptProjectFinal/RecieptProjectFinal/Trends/TrendsView.swift
+++ RecieptProjectFinal/RecieptProjectFinal/Trends/TrendsView.swift
@@ -11,14 +11,14 @@ import SwiftUICharts
 struct TrendsView: View {
     
     @Binding var barchartdata: [(String, Int)]
+    @Binding var linegraphdata: [Double]
     
     var body: some View {
         ZStack{
             Color(.black).ignoresSafeArea(.all)
             VStack{
-                LineChartView(data: [8,23,54,32,12,37,7,23,43], title: "Quantity of Items")
+                LineChartView(data: linegraphdata, title: "Quantity of Items")
                 BarChartView(data: ChartData(values: barchartdata), title: "Quantity of Each Item")
-                PieChartView(data: [8,23,54,32], title: "Title")
             }
         }
     }
